9677ab589342dcd6f384b6406a77cf6f1fb9f5d9
Cory <corymclean0@gmail.com>
2023-10-08 14:18:44 -0400

    ugh

--- main.js
+++ main.js
@@ -4,6 +4,98 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+/*const hearts = document.querySelectorAll('.like-glyph')
+hearts.forEach((item) => {
+  if(item.textContent = FULL_HEART){
+    item.addEventListener('click', (event) => {
+      event.target.textContent = EMPTY_HEART
+      event.target.classList.remove('activated-heart')
+    })
+  }else if(item.textContent = EMPTY_HEART){
+    item.addEventListener('click', (event) => {
+      mimicServerCall("http://mimicServer.example.com")
+  .then((res) => {
+    event.target.textContent = FULL_HEART
+    event.target.classList.add("activated-heart")
+  })
+  .catch((error) => {
+    const errmsg = document.querySelector('#modal')
+    errmsg.className = " "
+    setTimeout(() => errmsg.className = "hidden", 3000)
+  })
+    })
+  }})*/
+
+  const hearts = document.querySelectorAll('.like-glyph')
+
+  hearts.forEach((heart) => {
+    heart.addEventListener('click', () => {
+      if (heart.textContent === EMPTY_HEART){
+        mimicServerCall()
+        .then((res) => {
+          heart.textContent = FULL_HEART
+          heart.classList.add('activated-heart')
+        })
+        .catch((error) => {
+          const errmsg = document.querySelector('#modal')
+          errmsg.className = " "
+          setTimeout(() => errmsg.className = "hidden", 3000)
+        })
+      } else {
+        heart.textContent = EMPTY_HEART
+        heart.classList.remove('activated-heart')
+      }
+    })})
+  
+  /*function nono(e) {
+    
+  }
+
+function gogo(e) {
+  const heart = e.target
+  mimicServerCall()
+    .then((res) => {
+      heart.innerText = FULL_HEART
+      heart.classList.add('activated-heart')
+    })
+    .catch((error) => {
+      const errmsg = document.querySelector('#modal')
+      errmsg.className = " "
+      setTimeout(() => errmsg.className = "hidden", 3000)
+    })
+}
+
+
+
+for (const item of hearts){
+  if(item.innerText === FULL_HEART){
+    item.addEventListener('click', nono)
+  } else{
+  item.addEventListener('click', gogo)
+  }
+}
+const learts = document.getElementsByClassName('activated-hearts')*/
+
+/*const hearts = document.querySelectorAll('.like-glyph')
+hearts.forEach((item) => item.addEventListener('click', (e) => {
+  mimicServerCall("http://mimicServer.example.com")
+  .then((res) => {
+    e.target.innerHTML = FULL_HEART
+    e.target.classList.add("activated-heart")
+  })
+  .catch((error) => {
+    const errmsg = document.querySelector('#modal')
+    errmsg.className = " "
+    setTimeout(() => errmsg.className = "hidden", 3000)
+  })
+}))
+
+hearts.forEach((item) => item.addEventListener('click', (e) => {
+if(item.classList[1]){
+  return item.classList.remove('activated-heart')
+}
+}))*/
+
 
 
 
